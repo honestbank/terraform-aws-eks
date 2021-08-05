a2953a94039f3de808fb2b4c9feca2d5c6f185b3
Peter Griffin <peter.griffin@honestbank.com>
2021-08-05 17:19:57 +0700

    chore: fix map_users and map_roles

--- eks.tf
+++ eks.tf
@@ -25,11 +25,15 @@ module "eks" {
     }
   }
 
+  map_users = [
+    { "groups" : ["system:masters"], "userarn" : "arn:aws:iam::033245014990:user/jai", "username" : "jai" },                    # jai
+    { "groups" : ["system:masters"], "userarn" : "arn:aws:iam::033245014990:user/peter.griffin", "username" : "peter.griffin" } # peter.griffin
+  ]
 
-  map_users        = var.map_users
-  map_roles        = var.map_roles
-  write_kubeconfig = false # No point writing it in the cloud, lets get it from AWS directly afterwards
-  # kubeconfig_output_path = "./"
+  map_roles = [
+    { "groups" : ["system:masters"], "rolearn" : "arn:aws:iam::106256755710:role/OrganizationAccountAccessRole", "username" : "cross-account" } # Cross Account Role
+  ]
+
 }
 
 data "aws_eks_cluster" "default" {
