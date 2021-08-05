761d5d7be21ec02612c7a83e73ddf3c3ce4c2342
pgcloud <77714312+pgcloud@users.noreply.github.com>
2021-08-05 13:34:32 +0700

    chore: remove exposed (revoked) credentials (#2)
    * chore: remove exposed (revoked) credentials
    
    * chore: fix terratest
    
    * fix terratest take 2
    
    * remove errorenous pause command in run.sh

--- test/aws-eks_test.go
+++ test/aws-eks_test.go
@@ -18,7 +18,7 @@ func TestTerraformAwsEKS(t *testing.T) {
 	t.Parallel()
 
 	name := fmt.Sprintf("labs-eks-terratest-%s", random.UniqueId())
-	private_subnets := []string{"subnet-05d6411aa893da0f5", "subnet-08f37f5a0bf4494e7", "subnet-0c4aff30b966be038"}
+	private_subnets := []string{"subnet-000f3cf76c4b7782f", "subnet-077774cfd19cc9c7a", "subnet-0b5dcfd951cfaa94b"}
 	map_users := []interface{}{
 		map[string]interface{}{
 			"groups":   []string{"system:masters"},
@@ -51,7 +51,7 @@ func TestTerraformAwsEKS(t *testing.T) {
 		Vars: map[string]interface{}{
 			"name":                 name,
 			"kubernetes_version":   "1.19",
-			"vpc_id":               "vpc-012c1acb51eb87c20", // Pre-provisioned test VPC, specifically for EKS tests.
+			"vpc_id":               "vpc-0fc178a397554ab75", // Pre-provisioned test VPC, specifically for EKS tests.
 			"private_subnets":      private_subnets,
 			"eks_min_capacity":     3,
 			"eks_max_capacity":     3,
