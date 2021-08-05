d7c5e9069ab3828ff83ea2db34268b7d7297bde5
Peter Griffin <peter.griffin@honestbank.com>
2021-08-05 19:46:02 +0700

    update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,17 +1,3 @@
-data "aws_eks_cluster" "eks_cluster" {
-  name = module.amazon_eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "eks_cluster" {
-  name = module.amazon_eks.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster.token
-}
-
 module "amazon_eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "17.1.0"
@@ -31,13 +17,5 @@ module "amazon_eks" {
     }
   }
 
-  map_users = [
-    { "groups" : ["system:masters"], "userarn" : "arn:aws:iam::033245014990:user/jai", "username" : "jai" },                    # jai
-    { "groups" : ["system:masters"], "userarn" : "arn:aws:iam::033245014990:user/peter.griffin", "username" : "peter.griffin" } # peter.griffin
-  ]
-
-  map_roles = [
-    { "groups" : ["system:masters"], "rolearn" : "arn:aws:iam::106256755710:role/OrganizationAccountAccessRole", "username" : "cross-account" } # Cross Account Role
-  ]
-
+  manage_aws_auth = false
 }
