70cf6e6b03dbef2d6d1357922d7bef277d07c53d
Peter Griffin <peter.griffin@honestbank.com>
2021-08-11 15:04:12 +0700

    chore: update terratest

--- test/aws-eks_test.go
+++ test/aws-eks_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/eks"
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
@@ -16,9 +15,6 @@ import (
 
 func TestTerraformAwsEKS(t *testing.T) {
 	t.Parallel()
-
-	name := fmt.Sprintf("labs-eks-terratest-%s", random.UniqueId())
-	private_subnets := []string{"subnet-000f3cf76c4b7782f", "subnet-077774cfd19cc9c7a", "subnet-0b5dcfd951cfaa94b"}
 
 	workingDir := test_structure.CopyTerraformFolderToTemp(t, "../.", ".")
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
