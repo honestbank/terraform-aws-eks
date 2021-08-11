ebd7b321470e9e2bbc9ba1d9eb51abbadcb0687a
Jai Govindani <jai@honestbank.com>
2021-08-11 23:24:07 +0700

    test: add k8s provider to eks.tf module

--- eks/eks.tf
+++ eks/eks.tf
@@ -1,9 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.52.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.4.1"
+    }
+  }
+}
+
 data "aws_eks_cluster" "eks-cluster" {
   name = module.amazon_eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "eks-cluster" {
   name = module.amazon_eks.cluster_id
+}
+
+provider "kubernetes" {
+  host = data.aws_eks_cluster.eks-cluster.endpoint
+
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks-cluster.token
 }
 
 module "amazon_eks" {
