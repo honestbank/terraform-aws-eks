5c20f842098a6028de6119a78d0e0735eea3ad01
pgcloud <77714312+pgcloud@users.noreply.github.com>
2021-08-11 15:40:55 +0700

    chore: refactor terraform-aws-eks submodule (#12)
    * chore: refactor terraform-aws-eks submodule
    
    * fix instance type in terratest
    
    * add terraform.yml
    
    * update outputs.tf
    
    * chore: damn github action needing terraform init
    
    * chore: TFC backend doesn't like manually applied vars in terratest. Rely on the *.auto.tfvars file instead
    
    * chore: update terratest
    
    * chore: update terratest
    
    * chore: update terratest
    
    * chore: remove unused imports
    
    * chore: update terratest

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -1,19 +1,19 @@
 output "eks-cluster-endpoint" {
   description = "The URI of the cluster endpoint, used for Admin tasks, i.e Kubectl"
-  value       = module.eks.eks-cluster-endpoint
+  value       = data.aws_eks_cluster.eks-cluster.endpoint
 }
 output "eks-cluster-token" {
   description = "Token to access the EKS cluster"
-  value       = module.eks.eks-cluster-token
+  value       = data.aws_eks_cluster_auth.eks-cluster.token
   sensitive   = true
 }
 
 output "certificate_authority_data" {
   description = "CA data used to connect to the cluster. Outputted to allow the Kubernetes provider to install the aws_auth config map (allow allocated users to access)"
-  value       = module.eks.certificate_authority_data
+  value       = data.aws_eks_cluster.eks-cluster.certificate_authority.0.data
 }
 
 output "cluster_id" {
   description = "Tied to the internal cluster_id of the AWS EKS module"
-  value       = module.eks.cluster_id
+  value       = module.amazon_eks.cluster_id
 }
--- test.tfvars
+++ test.tfvars
@@ -24,11 +24,11 @@
 #     EKS config itself, as it's applied to Kubernetes directly. Without access rights, we cannot edit this file.
 
 name               = "labs-test-vpc-deployment"
-kubernetes_version = 1.19
+kubernetes_version = 1.20
 vpc_id             = "vpc-012c1acb51eb87c20" # Pre-existing test VPC, specifically for EKS tests. 
 private_subnets    = ["subnet-05d6411aa893da0f5", "subnet-08f37f5a0bf4494e7", "subnet-0c4aff30b966be038"]
 
 eks_min_capacity     = 3
 eks_desired_capacity = 3
 eks_max_capacity     = 3
-eks_instance_type    = "m5.medium"
+eks_instance_type    = "m5.large"
