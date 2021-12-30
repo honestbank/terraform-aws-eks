717af01b9ac2ef71eb8063d80d506dfbedfb5b91
Jai Govindani <jai@honestbank.com>
2021-12-30 21:53:32 +0700

    fix:version constraints

--- remote-backend.tf
+++ remote-backend.tf
@@ -6,17 +6,17 @@ terraform {
       name = "terraform-aws-eks"
     }
   }
-  required_version = "~> 1.0.0"
+  required_version = "~> 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.52.0"
+      version = "~> 3.52"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.4.1"
+      version = "~> 2.4"
     }
   }
 }
